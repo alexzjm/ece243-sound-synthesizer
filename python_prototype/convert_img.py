57cfb4833d207b055289bd9893e4540d0a030c7e
Ken-2511 <IWMAIN@outlook.com>
2025-03-18 22:57:07 -0400

    Refactor image conversion function to improve clarity and functionality

--- python_prototype/convert_img.py
+++ python_prototype/convert_img.py
@@ -5,33 +5,51 @@ import cv2
 import pyperclip
 
 
-def convert_img(img_path, img_name, width, height):
-    img = cv2.imread(img_path)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# this function converts the image to the c-style array
+# the image is first resized to the specified width and height
+# the image is then converted to short unsigned int color with 5 bits for red, 6 bits for green, and 5 bits for blue
+# the image is then flattened to a 1D array
+# the image is then copied to the clipboard
+def convert_img(file_name, img_name, width, height):
+    img = cv2.imread(file_name)
     img = cv2.resize(img, (width, height))
-    img = img / 255
-    img = np.round(img)
-    img = img.astype(np.int8)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    img_r = img[:, :, 0] >> 3
+    img_g = img[:, :, 1] >> 2
+    img_b = img[:, :, 2] >> 3
+    img = (img_r << 11) + (img_g << 5) + img_b
     img = img.flatten()
-    img = img.tolist()
-    img = ', '.join([str(i) for i in img])
-    # add the beginning and the end of the array
-    img = f"int8_t {img_name}[{width * height}] = {{" + img + "};"
-    # add the draw_image function
-    img += "\n\n"
-    img += generate_draw_pixel_code(img_name, width, height, 255)
-    pyperclip.copy(img)
+    answer = ', '.join(map(str, img))
+    answer = f"short unsigned int {img_name}[{width * height}] = {{{answer}}};"
+
+    answer += "\n\n"
+    answer += generate_draw_pixel_code(img_name, width, height, img_name)
+
+    pyperclip.copy(answer)
+    print(answer)
 
 
+# This is the function that generates the plot_image function
+# this function utilizes the plot_pixel function
+# the signature of the plot_pixel function is assumed to be void plot_pixel(int x, int y, short unsigned int color);
+# the signature of the plot_image function is assumed to be void plot_image_img_name(int x, int y);
 def generate_draw_pixel_code(img_name, width, height, color):
-    code_lines = []
-    for y in range(height):
-        for x in range(width):
-            code_lines.append(f"    draw_pixel({x}, {y}, {color} * {img_name}[{y * width + x}]);")
-    code = "\n".join(code_lines)
-    c_code = f"void draw_image() {{\n{code}\n}}"
-    return c_code
+    code = f"""
+void plot_image_{img_name}(int x, int y) {{
+    for (int i = 0; i < {height}; i++) {{
+        for (int j = 0; j < {width}; j++) {{
+            plot_pixel(x + j, y + i, {color}[i * {width} + j]);
+        }}
+    }}
+}}
+"""
+    return code
 
 
 if __name__ == '__main__':
-    convert_img('images/sine.png', 'sine_img', 20, 10)
+    convert_img(
+        file_name='images/sine.png',
+        img_name='img_sine',
+        width=20,
+        height=10
+    )
