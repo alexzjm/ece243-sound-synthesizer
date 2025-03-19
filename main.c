44db0a76a1262bab204bd1da00a525de6e00a73d
alexzjm <164368835+alexzjm@users.noreply.github.com>
2025-03-18 20:10:55 -0400

    setup pointers to locations of i/o registers

--- main.c
+++ main.c
@@ -1,5 +1,31 @@
 #include <stdio.h>
+#include <stdbool.h>
+
+#define AUDIO_BASE 0xFF203040
+#define KEY_BASE 0xFF200050
+#define SWITCH_BASE 0xFF200040
+
+typedef struct audio_s {
+    volatile unsigned int CTRL;
+    volatile unsigned char RARC, RALC, WSRC, WSLC;
+    volatile unsigned int LDATA;
+    volatile unsigned int RDATA;
+} audio_s;
+
+typedef struct parallel_port {
+    volatile unsigned int DR;
+    volatile unsigned int DIR;
+    volatile unsigned int MASK;
+    volatile unsigned int EDGE;
+} parallel_port;
 
 void main () {
-    print("chengyongkang");
+
+    audio_s *audio_ptr = (audio_s *)AUDIO_BASE;
+    parallel_port *key_ptr = (parallel_port *)KEY_BASE;
+    parallel_port *sw_ptr = (parallel_port *)SWITCH_BASE;
+
+    while (1) {
+        
+    }
 }
