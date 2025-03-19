9e0733fb5a4b452b9e25d0123ec6e4bddf57591c
alexzjm <164368835+alexzjm@users.noreply.github.com>
2025-03-18 20:28:47 -0400

    initialized struct and ptr for ps2 keyboard

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #define AUDIO_BASE 0xFF203040
 #define KEY_BASE 0xFF200050
 #define SWITCH_BASE 0xFF200040
+#define PS2_BASE 0xFF200100
 
 typedef struct audio_s {
     volatile unsigned int CTRL;
@@ -19,13 +20,21 @@ typedef struct parallel_port {
     volatile unsigned int EDGE;
 } parallel_port;
 
+typedef struct ps2_port {
+    volatile unsigned int DATA;
+    volatile unsigned int CTRL;
+} ps2_port;
+
 void main () {
 
     audio_s *audio_ptr = (audio_s *)AUDIO_BASE;
     parallel_port *key_ptr = (parallel_port *)KEY_BASE;
     parallel_port *sw_ptr = (parallel_port *)SWITCH_BASE;
+    ps2_port *ps2_ptr = (ps2_port *)PS2_BASE;
 
     while (1) {
-        
+
     }
+
+    return 0;
 }
