6d184686e11c70e089e3ba90c26cfade7e50dc2c
Ken-2511 <IWMAIN@outlook.com>
2025-03-19 14:59:05 -0400

    Enhance image conversion function with improved documentation and output generation

--- python_prototype/convert_img.py
+++ python_prototype/convert_img.py
@@ -1,49 +1,62 @@
-# this file is for converting the image to the format of c-style array
-
 import numpy as np
 import cv2
 import pyperclip
 
 
-# this function converts the image to the c-style array
-# the image is first resized to the specified width and height
-# the image is then converted to short unsigned int color with 5 bits for red, 6 bits for green, and 5 bits for blue
-# the image is then flattened to a 1D array
-# the image is then copied to the clipboard
 def convert_img(file_name, img_name, width, height):
+    """
+    Convert an image to a C-style array.
+
+    The image is resized, converted to a 16-bit color format (5-6-5 RGB), flattened,
+    and copied to the clipboard as a C array.
+
+    :param file_name: Path to the input image.
+    :param img_name: Name of the output C array.
+    :param width: Target width of the image.
+    :param height: Target height of the image.
+    """
     img = cv2.imread(file_name).astype(np.uint16)
     img = cv2.resize(img, (width, height), interpolation=cv2.INTER_NEAREST)
-    # img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    
+    # Convert BGR to 5-bit Red, 6-bit Green, and 5-bit Blue
     img_r = img[:, :, 2] >> 3
     img_g = img[:, :, 1] >> 2
     img_b = img[:, :, 0] >> 3
-    print(img_r[0, 0], img_g[0, 0], img_b[0, 0])
+    
+    # print("First pixel values:", img_r[0, 0], img_g[0, 0], img_b[0, 0])
+    
     img = (img_r << 11) + (img_g << 5) + img_b
-    print(img[0, 0])
+    # print("Converted first pixel value:", img[0, 0])
+    
     img = img.flatten()
-    answer = ', '.join(map(lambda x: f"0x{x:04X}", img))
-    answer = f"short unsigned int {img_name}[{width * height}] = {{{answer}}};"
+    img_array = ', '.join(f"0x{x:04X}" for x in img)
+    output_code = f"short unsigned int {img_name}[{width * height}] = {{{img_array}}};"
+    
+    # Generate and append drawing function code
+    output_code += "\n\n" + generate_draw_pixel_code(img_name, width, height)
+    
+    # Copy to clipboard
+    pyperclip.copy(output_code)
+    print("C array copied to clipboard.")
 
-    answer += "\n\n"
-    answer += generate_draw_pixel_code(img_name, width, height, img_name)
 
-    pyperclip.copy(answer)
+def generate_draw_pixel_code(img_name, width, height):
+    """
+    Generate C functions to plot and erase an image using plot_pixel().
 
-
-# This is the function that generates the plot_image function
-# this function utilizes the plot_pixel function
-# the signature of the plot_pixel function is assumed to be void plot_pixel(int x, int y, short unsigned int color);
-# the signature of the plot_image function is assumed to be void plot_image_img_name(int x, int y);
-def generate_draw_pixel_code(img_name, width, height, color):
-    code = f"""
+    :param img_name: Name of the image array.
+    :param width: Image width.
+    :param height: Image height.
+    :return: C function code as a string.
+    """
+    return f"""
 void plot_image_{img_name}(int x, int y) {{
     for (int i = 0; i < {height}; i++) {{
         for (int j = 0; j < {width}; j++) {{
-            plot_pixel(x + j, y + i, {color}[i * {width} + j]);
+            plot_pixel(x + j, y + i, {img_name}[i * {width} + j]);
         }}
     }}
 }}
-
 
 void erase_image_{img_name}(int x, int y) {{
     for (int i = 0; i < {height}; i++) {{
@@ -53,13 +66,12 @@ void erase_image_{img_name}(int x, int y) {{
     }}
 }}
 """
-    return code
 
 
 if __name__ == '__main__':
     convert_img(
-        file_name='images/sine.png',
-        img_name='sine',
+        file_name='images/triangle.png',
+        img_name='triangle',
         width=50,
         height=20
     )
